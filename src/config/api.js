d240cf61f77398e661868f03713bca54e7be66fc
engkos <engkoskostaman759@gmail.com>
2022-10-04 01:37:04 +0700

    deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// export const API = axios.create({
+//     baseURL: "http://localhost:5000/api/v1/",
+// });
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1/",
+    baseURL: process.env.REACT_APP_BASEURL,
 });
 
 export const setAuthToken = (token) => {
